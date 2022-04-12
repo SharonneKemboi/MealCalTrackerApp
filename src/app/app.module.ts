7a20b5f8a3142ea5f5c41f8818f1db90b47ed46a
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-04-12 21:42:27 +0300

    Generate a Meal component and a meal class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Meal } from './meal.ts/meal.ts.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Meal.TsComponent
+    AppComponent
+  
   ],
   imports: [
     BrowserModule,
